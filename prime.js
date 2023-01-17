84f456d70c44235e5cd76a1feca4313056144cb2
Keertika Jauhar <jauharricha@gmail.com>
2023-01-17 21:41:38 +0530

    prime

---
+++ prime.js
@@ -0,0 +1,20 @@
+// check given number prime or not if it prime print yes else no;
+
+
+let num=13;
+
+let count=0;
+
+for(let i=1 ; i<=num ; i++){
+  
+if(num%i==0){
+   count++;
+ }
+}
+
+if(count==2){
+  console.log("Yes");
+}else{
+  console.log("No");
+}
+ 
